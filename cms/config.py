89282e513ebc5299668ea97622da21ec66861ae4
xenu-xore <52954685+xenu-xore@users.noreply.github.com>
2020-12-24 12:56:27 +0300

    Добавлено редактирование категорий, исключение для slug_art/cat

--- cms/config.py
+++ cms/config.py
@@ -17,8 +17,7 @@ class DevConfig(BaseConfig):
     DEBUG = True
     WTF_CSRF_ENABLED = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dev.sqlite')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://flask_user:19902273@localhost/flasklogin_db'
-    DEBUG_TB_ENABLED = False
+    DEBUG_TB_ENABLED = True
     STRIPE_SECRET_KEY = 'foo'
     STRIPE_PUBLISHABLE_KEY = 'bar'
 
@@ -27,7 +26,6 @@ class ProConfig(BaseConfig):
     SECRET_KEY = 'nadezda'
     DEBUG = True
     WTF_CSRF_ENABLED = True
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dev.sqlite')
     SQLALCHEMY_DATABASE_URI = 'postgresql://flask_user:19902273@localhost/flasklogin_db'
     DEBUG_TB_ENABLED = False
     STRIPE_SECRET_KEY = 'foo'
